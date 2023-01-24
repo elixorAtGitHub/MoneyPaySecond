321ad643bac35833db490eb8d1d3a3b7497f5490
Aleksandr Shishkin <elixor@mail.ru>
2023-01-24 23:06:41 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,27 @@
+import kotlin.math.max
+
+const val TYPE_MASTERCARD = "MasterCard"
+const val TYPE_MAESTRO = "Maestro"
+const val TYPE_VISA = "Visa"
+const val TYPE_MIR = "Мир"
+const val TYPE_VKPAY = "VK Pay"
+const val ERROR_TYPE = -1
+const val ERROR_LIMIT = -2
+
+fun main() {
+    val transactionAmmount = 10000
+    val previousTransactions = 60000
+    val text1 = "Если ранее в течении месяца отправлялось через "
+    val text2 = " " + (previousTransactions).toString() + ", то комиссия для $transactionAmmount составит "
+    println(text1 + TYPE_VISA + text2 + comission(TYPE_VISA, transactionAmmount, previousTransactions))
+    println(text1 + TYPE_MIR + text2 + comission(TYPE_MIR, transactionAmmount, previousTransactions))
+    println(text1 + TYPE_MASTERCARD + text2 + comission(TYPE_MASTERCARD, transactionAmmount, previousTransactions))
+    println(text1 + TYPE_MAESTRO + text2 + comission(TYPE_MAESTRO, transactionAmmount, previousTransactions))
+    println(text1 + TYPE_VKPAY + text2 + comission(TYPE_VKPAY, transactionAmmount, previousTransactions))
+}
+fun comission(typeCard: String, amount: Int, previous: Int): Int = when (typeCard) {
+    "Visa", "Мир" -> if (amount <= 150_000 && amount+previous <= 600_000) max(35, (amount * 0.0075f).toInt()) else ERROR_LIMIT
+    "MasterCard", "Maestro" -> if (amount <= 150_000 && amount+previous <= 600_000) 0 else ERROR_LIMIT
+    "VK Pay" -> 0
+    else -> ERROR_TYPE
+}
